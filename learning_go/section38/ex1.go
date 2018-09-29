8452cc6ae1a7eb6ee018f7fd3193f52dd2a54e20
Averill Cate, Jr <acatejr@gmail.com>
2018-09-29 10:35:03 -0700

    Excercises work.

---
+++ learning_go/section38/ex1.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Excercise 1.")
+	fmt.Println(half(1))
+	fmt.Println(half(2))
+
+	// Excercise 2
+	doHalf := func(n int) (int, bool) {
+		isEven := (n%2 == 0)
+		result := n / 2
+		return result, isEven
+	}
+	fmt.Println("\n")
+	fmt.Println(doHalf(1))
+	fmt.Println(doHalf(2))
+
+	// Excercise 3 - find the greatest integer in list
+	fmt.Println("\n")
+	fmt.Printf("The max is %d\n", findGreatest(1, 2, 3))
+	fmt.Printf("The max is %d\n", findGreatest(3, 99, 3))
+	fmt.Printf("The max is %d\n", findGreatest(13, 459, 7, 2, 4, 53, 73, 25, 3))
+
+	// Excercise 4 what is the value of:
+	// (true && false) || (false && true) || !(false && fale)
+	fmt.Println("\n")
+	fmt.Printf("%t", expressionTest())
+
+	// Excercise 5 - function foo called in many ways
+	fmt.Println("\n")
+	foo(1, 2)
+	foo(1, 2, 3)
+	aSlice := []int{1, 2, 3, 4}
+	foo(aSlice...)
+	foo()
+
+}
+
+func foo(args ...int) {
+	fmt.Println(args)
+}
+
+func expressionTest() bool {
+	return (true && false) || (false && true) || !(false && false)
+}
+
+func findGreatest(numList ...int) int {
+	var result int
+	for v := range numList {
+		if numList[v] > result {
+			result = numList[v]
+		}
+	}
+	return result
+}
+
+// Excercise 1
+func half(n int) (int, bool) {
+	isEven := (n%2 == 0)
+	result := n / 2
+	return result, isEven
+}
